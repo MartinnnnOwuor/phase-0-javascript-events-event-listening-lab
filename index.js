1e30875d84622cd0d53f78890f2654cfb36dd878
martin-XPS-15-9510 <oowuoroowuor@gmail.com>
2024-04-14 22:09:17 +0300

    submit event listener lab

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 function addingEventListener() {
+    const button = document.getElementById('button');
+    button.addEventListener('click', function(event) {
+        // Handle the click event here
+        console.log('Button clicked!');
+    });
 }
+
+
+
